393810391bd89e1a9a45a2051350fd90bf76fc94
Buddika Abeykoon <59583095+buddika-sandMan@users.noreply.github.com>
2024-11-20 11:27:26 +0530

    Update TestGreeter.java

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -19,7 +19,7 @@ public class TestGreeter {
   public void setup() {
     greeter = new Greeter();
   }
-
+;
   @Test
   public void greetShouldIncludeTheOneBeingGreeted() {
     String someone = "World";
